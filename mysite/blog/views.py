daebd18b088acf4eeb965779613cf8a21bf763e8
Kangjik Kim <rkqqkb@gmail.com>
2025-01-23 23:31:37 +0900

    게시글 상세 뷰에 댓글 추가

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -32,8 +32,10 @@ def post_list(request):
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post,
                              publish__year=year, publish__month=month, publish__day=day)
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
 
-    return render(request, 'blog/post/detail.html', {'post': post})
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form})
 
 
 def post_share(request, post_id):
